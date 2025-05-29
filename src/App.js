a1b138d5401316161dd1f6675d5b2918fd919a81
Vraj Prajapati <vrajprajapati28506@gmail.com>
2025-05-29 20:51:33 +0530

    PreEventPage Changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import AboutUs from './components/AboutUs';
 import PreEventsSection from './components/PreEventsSection';
 import Poster from './components/Poster';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
+import PreEventInfo from './components/PreEventInfo';
 import './App.css';
 
-function App() {
+// Main page component
+const MainPage = () => {
   return (
-    <div className="App">
+    <>
       <Navbar />
       <div id="home">
         <Hero />
@@ -23,6 +27,26 @@ function App() {
         <Poster />
       </div>
       <Footer />
+    </>
+  );
+};
+
+// Event info page component
+const EventInfoPage = () => {
+  return (
+    <PreEventInfo />
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/event/:eventId" element={<EventInfoPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
